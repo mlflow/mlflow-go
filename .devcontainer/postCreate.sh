752db979cf39d7d528ebabcd212bbb1daa2360cf
nojaf <florian.verdonck@outlook.com>
2024-07-02 16:16:57 +0000

    Less bash sacrilege, move mage file to magefiles folder.
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,10 +1,7 @@
-!#/bin/sh
+#!/bin/sh
 
 # Install precommit (https://pre-commit.com/)
 pre-commit install -t pre-commit -t prepare-commit-msg
-
-# Download Go dependencies
-go mod tidy
 
 # Install Mage (https://magefile.org/)
 go install github.com/magefile/mage@v1.15.0
