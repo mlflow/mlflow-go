9d8150db54192953664be6d306915afb97aff5dc
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2024-07-02 17:31:35 +0100

    Keep Go cache across devcontainer rebuilds
    Signed-off-by: Jonathan Giannuzzi <jonathan@giannuzzi.me>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Fix permissions for the Go cache
+sudo chown -R $(id -u):$(id -g) /var/cache/go
+
 # Install precommit (https://pre-commit.com/)
 pre-commit install -t pre-commit -t prepare-commit-msg
 
