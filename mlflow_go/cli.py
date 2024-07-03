36227096b32fafc133b85d1d8fafc8a856771f91
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2024-07-03 17:59:56 +0100

    Allow passing python_address and python_command to the Go server
    Signed-off-by: Jonathan Giannuzzi <jonathan@giannuzzi.me>

--- mlflow_go/cli.py
+++ mlflow_go/cli.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+import shlex
 import socket
 
 import click
@@ -26,34 +27,46 @@ def _get_commands():
         go_opts,
         **kwargs,
     ):
-        # assign a random port for the Python server
-        python_args = kwargs.copy()
-        python_args.update(
-            {
-                "host": "127.0.0.1",
-                "port": str(_get_safe_port()),
-            }
-        )
-
-        # construct the Python server command
-        python_command = [
-            "mlflow",
-            "server",
-        ]
-        for key, value in python_args.items():
-            if isinstance(value, bool):
-                if value:
-                    python_command.append(f"--{key.replace('_', '-')}")
-            elif value is not None:
-                python_command.append(f"--{key.replace('_', '-')}")
-                python_command.append(str(value))
-
         # convert the Go options to a dictionary
         opts = {}
         if go_opts:
             for opt in go_opts.split(","):
                 key, value = opt.split("=", 1)
                 opts[key] = value
+
+        # validate the Python server configuration if set
+        if ("python_address" in opts) ^ ("python_command" in opts):
+            raise click.ClickException("python_address and python_command have to be set together")
+
+        if "python_address" and "python_command" in opts:
+            # use the provided Python server configuration
+            python_address = opts["python_address"]
+            python_command = shlex.split(opts["python_command"])
+        else:
+            # assign a random port for the Python server
+            python_host = "127.0.0.1"
+            python_port = _get_safe_port()
+            python_address = f"{python_host}:{python_port}"
+            python_args = kwargs.copy()
+            python_args.update(
+                {
+                    "host": python_host,
+                    "port": python_port,
+                }
+            )
+
+            # construct the Python server command
+            python_command = [
+                "mlflow",
+                "server",
+            ]
+            for key, value in python_args.items():
+                if isinstance(value, bool):
+                    if value:
+                        python_command.append(f"--{key.replace('_', '-')}")
+                elif value is not None:
+                    python_command.append(f"--{key.replace('_', '-')}")
+                    python_command.append(str(value))
 
         # initialize the Go server configuration
         tracking_store_uri = kwargs["backend_store_uri"]
@@ -63,7 +76,7 @@ def _get_commands():
                 kwargs["serve_artifacts"], kwargs["default_artifact_root"], tracking_store_uri
             ),
             "log_level": opts.get("log_level", "DEBUG" if kwargs["dev"] else "INFO"),
-            "python_address": f"""{python_args["host"]}:{python_args["port"]}""",
+            "python_address": python_address,
             "python_command": python_command,
             "shutdown_timeout": opts.get("shutdown_timeout", "1m"),
             "static_folder": pathlib.Path(mlflow.server.__file__)
