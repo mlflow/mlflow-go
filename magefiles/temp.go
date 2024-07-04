2e1daf947d51e23fbfdfbfb034648d174e3ec3c9
nojaf <florian.verdonck@outlook.com>
2024-07-04 17:04:56 +0100

    Moar mage targets
    https://github.com/jgiannuzzi/mlflow-go/pull/4
    
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

---
+++ magefiles/temp.go
@@ -0,0 +1,74 @@
+//go:build mage
+
+//nolint:wrapcheck
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+func pipInstall(args ...string) error {
+	allArgs := append([]string{"install"}, args...)
+
+	return sh.RunV("pip", allArgs...)
+}
+
+func tar(args ...string) error {
+	return sh.RunV("tar", args...)
+}
+
+func Temp() error {
+	mg.Deps(Repo.Init)
+
+	// Install our Python package and its dependencies
+	if err := pipInstall("-e", "."); err != nil {
+		return err
+	}
+
+	// Install the dreaded psycho
+	if err := pipInstall("psycopg2-binary"); err != nil {
+		return err
+	}
+
+	// Archive the MLFlow pre-built UI
+	if err := tar(
+		"-C", "/usr/local/python/current/lib/python3.8/site-packages/mlflow",
+		"-czvf",
+		"./ui.tgz",
+		"./server/js/build",
+	); err != nil {
+		return err
+	}
+
+	mlflowRepoPath, err := filepath.Abs(MLFlowRepoFolderName)
+	if err != nil {
+		return err
+	}
+
+	// Add the UI back to it
+	if err := tar(
+		"-C", mlflowRepoPath,
+		"-xzvf", "./ui.tgz",
+	); err != nil {
+		return err
+	}
+
+	// Remove tar file
+	tarPath, err := filepath.Abs("ui.tgz")
+	if err != nil {
+		return err
+	}
+
+	defer os.Remove(tarPath)
+
+	// Install it in editable mode
+	if err := pipInstall("-e", mlflowRepoPath); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ magefiles/tests.go
@@ -0,0 +1,54 @@
+//go:build mage
+
+//nolint:wrapcheck
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+type Test mg.Namespace
+
+// Run mlflow Python tests against the Go backend.
+func (Test) Python() error {
+	mg.Deps(Generate)
+
+	libpath, err := os.MkdirTemp("", "")
+	if err != nil {
+		return err
+	}
+
+	// Remove the Go binary
+	defer os.RemoveAll(libpath)
+
+	// Build the Go binary in a temporary directory
+	if err := sh.RunV("python", "-m", "mlflow_go.lib", ".", libpath); err != nil {
+		return nil
+	}
+
+	//  Run the tests (currently just the server ones)
+	if err := sh.RunWithV(map[string]string{
+		"MLFLOW_GO_LIBRARY_PATH": libpath,
+	}, "pytest",
+		"--confcutdir=.",
+		".mlflow.repo/tests/tracking/test_rest_tracking.py",
+		".mlflow.repo/tests/tracking/test_model_registry.py",
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Run the Go unit tests.
+func (Test) Unit() error {
+	return sh.RunV("go", "test", "./pkg/...")
+}
+
+// Run all tests.
+func (Test) All() {
+	mg.Deps(Test.Unit, Test.Python)
+}
