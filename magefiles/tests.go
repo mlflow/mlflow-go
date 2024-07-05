53c592b55f2e0c006621f5f8c5777c4f8a46e318
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2024-07-05 16:27:19 +0100

    Add store tests
    Signed-off-by: Jonathan Giannuzzi <jonathan@giannuzzi.me>

--- magefiles/tests.go
+++ magefiles/tests.go
@@ -36,6 +36,8 @@ func (Test) Python() error {
 		"--confcutdir=.",
 		".mlflow.repo/tests/tracking/test_rest_tracking.py",
 		".mlflow.repo/tests/tracking/test_model_registry.py",
+		".mlflow.repo/tests/store/tracking/test_sqlalchemy_store.py",
+		".mlflow.repo/tests/store/model_registry/test_sqlalchemy_store.py",
 		"-k",
 		"not [file",
 	); err != nil {
