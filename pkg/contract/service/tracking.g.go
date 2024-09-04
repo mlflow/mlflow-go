95bb5d408c599400d2e1953bea7b31b2f12572fa
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-09-04 16:41:50 +0200

    *migrate `getRun` endpoint.
    (cherry picked from commit f0b969345e1290d8bad264d21b68064abf22bc90)

--- pkg/contract/service/tracking.g.go
+++ pkg/contract/service/tracking.g.go
@@ -4,8 +4,8 @@ package service
 
 import (
 	"context"
-	"github.com/mlflow/mlflow-go/pkg/protos"
 	"github.com/mlflow/mlflow-go/pkg/contract"
+	"github.com/mlflow/mlflow-go/pkg/protos"
 )
 
 type TrackingService interface {
@@ -18,6 +18,7 @@ type TrackingService interface {
 	CreateRun(ctx context.Context, input *protos.CreateRun) (*protos.CreateRun_Response, *contract.Error)
 	UpdateRun(ctx context.Context, input *protos.UpdateRun) (*protos.UpdateRun_Response, *contract.Error)
 	LogMetric(ctx context.Context, input *protos.LogMetric) (*protos.LogMetric_Response, *contract.Error)
+	GetRun(ctx context.Context, input *protos.GetRun) (*protos.GetRun_Response, *contract.Error)
 	SearchRuns(ctx context.Context, input *protos.SearchRuns) (*protos.SearchRuns_Response, *contract.Error)
 	LogBatch(ctx context.Context, input *protos.LogBatch) (*protos.LogBatch_Response, *contract.Error)
 }
--- pkg/server/routes/tracking.g.go
+++ pkg/server/routes/tracking.g.go
@@ -4,10 +4,10 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/mlflow/mlflow-go/pkg/server/parser"
 	"github.com/mlflow/mlflow-go/pkg/contract/service"
-	"github.com/mlflow/mlflow-go/pkg/utils"
 	"github.com/mlflow/mlflow-go/pkg/protos"
+	"github.com/mlflow/mlflow-go/pkg/server/parser"
+	"github.com/mlflow/mlflow-go/pkg/utils"
 )
 
 func RegisterTrackingServiceRoutes(service service.TrackingService, parser *parser.HTTPRequestParser, app *fiber.App) {
@@ -105,6 +105,17 @@ func RegisterTrackingServiceRoutes(service service.TrackingService, parser *pars
 			return err
 		}
 		output, err := service.LogMetric(utils.NewContextWithLoggerFromFiberContext(ctx), input)
+		if err != nil {
+			return err
+		}
+		return ctx.JSON(output)
+	})
+	app.Get("/mlflow/runs/get", func(ctx *fiber.Ctx) error {
+		input := &protos.GetRun{}
+		if err := parser.ParseQuery(ctx, input); err != nil {
+			return err
+		}
+		output, err := service.GetRun(utils.NewContextWithLoggerFromFiberContext(ctx), input)
 		if err != nil {
 			return err
 		}
