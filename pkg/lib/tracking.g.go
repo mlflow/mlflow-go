f0b969345e1290d8bad264d21b68064abf22bc90
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-09-04 16:41:50 +0200

    *migrate `getRun` endpoint.

--- pkg/lib/tracking.g.go
+++ pkg/lib/tracking.g.go
@@ -4,9 +4,10 @@ package main
 
 import "C"
 import (
-	"unsafe"
 	"github.com/mlflow/mlflow-go/pkg/protos"
+	"unsafe"
 )
+
 //export TrackingServiceGetExperimentByName
 func TrackingServiceGetExperimentByName(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -15,6 +16,7 @@ func TrackingServiceGetExperimentByName(serviceID int64, requestData unsafe.Poin
 	}
 	return invokeServiceMethod(service.GetExperimentByName, new(protos.GetExperimentByName), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceCreateExperiment
 func TrackingServiceCreateExperiment(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -23,6 +25,7 @@ func TrackingServiceCreateExperiment(serviceID int64, requestData unsafe.Pointer
 	}
 	return invokeServiceMethod(service.CreateExperiment, new(protos.CreateExperiment), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceGetExperiment
 func TrackingServiceGetExperiment(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -31,6 +34,7 @@ func TrackingServiceGetExperiment(serviceID int64, requestData unsafe.Pointer, r
 	}
 	return invokeServiceMethod(service.GetExperiment, new(protos.GetExperiment), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceDeleteExperiment
 func TrackingServiceDeleteExperiment(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -39,6 +43,7 @@ func TrackingServiceDeleteExperiment(serviceID int64, requestData unsafe.Pointer
 	}
 	return invokeServiceMethod(service.DeleteExperiment, new(protos.DeleteExperiment), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceRestoreExperiment
 func TrackingServiceRestoreExperiment(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -47,6 +52,7 @@ func TrackingServiceRestoreExperiment(serviceID int64, requestData unsafe.Pointe
 	}
 	return invokeServiceMethod(service.RestoreExperiment, new(protos.RestoreExperiment), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceUpdateExperiment
 func TrackingServiceUpdateExperiment(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -55,6 +61,7 @@ func TrackingServiceUpdateExperiment(serviceID int64, requestData unsafe.Pointer
 	}
 	return invokeServiceMethod(service.UpdateExperiment, new(protos.UpdateExperiment), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceCreateRun
 func TrackingServiceCreateRun(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -63,6 +70,7 @@ func TrackingServiceCreateRun(serviceID int64, requestData unsafe.Pointer, reque
 	}
 	return invokeServiceMethod(service.CreateRun, new(protos.CreateRun), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceUpdateRun
 func TrackingServiceUpdateRun(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -71,6 +79,7 @@ func TrackingServiceUpdateRun(serviceID int64, requestData unsafe.Pointer, reque
 	}
 	return invokeServiceMethod(service.UpdateRun, new(protos.UpdateRun), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceLogMetric
 func TrackingServiceLogMetric(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -79,6 +88,16 @@ func TrackingServiceLogMetric(serviceID int64, requestData unsafe.Pointer, reque
 	}
 	return invokeServiceMethod(service.LogMetric, new(protos.LogMetric), requestData, requestSize, responseSize)
 }
+
+//export TrackingServiceGetRun
+func TrackingServiceGetRun(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
+	service, err := trackingServices.Get(serviceID)
+	if err != nil {
+		return makePointerFromError(err, responseSize)
+	}
+	return invokeServiceMethod(service.GetRun, new(protos.GetRun), requestData, requestSize, responseSize)
+}
+
 //export TrackingServiceSearchRuns
 func TrackingServiceSearchRuns(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
@@ -87,6 +106,7 @@ func TrackingServiceSearchRuns(serviceID int64, requestData unsafe.Pointer, requ
 	}
 	return invokeServiceMethod(service.SearchRuns, new(protos.SearchRuns), requestData, requestSize, responseSize)
 }
+
 //export TrackingServiceLogBatch
 func TrackingServiceLogBatch(serviceID int64, requestData unsafe.Pointer, requestSize C.int, responseSize *C.int) unsafe.Pointer {
 	service, err := trackingServices.Get(serviceID)
