d53ead4233208008d5c0c41df3031d4b40751915
nojaf <florian.verdonck@outlook.com>
2024-10-03 13:16:47 +0000

    Do matrix thing with zig targets
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- magefiles/build.go
+++ magefiles/build.go
@@ -4,11 +4,10 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/magefile/mage/sh"
 )
@@ -17,34 +16,8 @@ const (
 	amd64 = "amd64"
 )
 
-var errUnknownTarget = errors.New("could not determine zig target")
-
-// Helper function to determine the Zig target triple based on OS and architecture.
-func getTargetTriple(goos, goarch string) (string, error) {
-	switch goos {
-	case "linux":
-		if goarch == amd64 {
-			return "x86_64-linux-gnu", nil
-		} else if goarch == "arm64" {
-			return "aarch64-linux-gnu", nil
-		}
-	case "darwin":
-		if goarch == amd64 {
-			return "x86_64-macos", nil
-		} else if goarch == "arm64" {
-			return "aarch64-macos", nil
-		}
-	case "windows":
-		if goarch == amd64 {
-			return "x86_64-windows-gnu", nil
-		}
-	}
-
-	return "", fmt.Errorf("%w: %s/%s", errUnknownTarget, goos, goarch)
-}
-
 // Build a Python wheel.
-func Build() error {
+func Build(target string) error {
 	env := "build/.venv"
 	if err := sh.RunV("python3", "-mvenv", env); err != nil {
 		return err
@@ -59,25 +32,25 @@ func Build() error {
 		return err
 	}
 
-	// Set Zig as the C compiler for cross-compilation
-	targetTriple, err := getTargetTriple(runtime.GOOS, runtime.GOARCH)
-	if err != nil {
-		return err
-	}
-
 	absPython, err := filepath.Abs(python)
 	if err != nil {
 		return err
 	}
 
-	zigCC := absPython + " -mziglang cc -target " + targetTriple
+	environmentVariables := map[string]string{
+		"GOOS":   runtime.GOOS,
+		"GOARCH": runtime.GOARCH,
+	}
+
+	// Set Zig as the C compiler for cross-compilation
+	// If we are on Mac and targeting Mac we don't need Zig.
+	if !(runtime.GOOS == "darwin" && strings.HasSuffix(target, "-macos")) {
+		zigCC := absPython + " -mziglang cc -target " + target
+		environmentVariables["CC"] = zigCC
+	}
 
 	if err := sh.RunWithV(
-		map[string]string{
-			"CC":     zigCC,
-			"GOOS":   runtime.GOOS,
-			"GOARCH": runtime.GOARCH,
-		},
+		environmentVariables,
 		python, "-mbuild"); err != nil {
 		return err
 	}
