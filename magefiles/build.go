5050dae97dc0f93bded2753c0a192f424e10e6e8
nojaf <florian.verdonck@outlook.com>
2024-10-03 14:04:43 +0000

    WIP: after talking to JoJo
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- magefiles/build.go
+++ magefiles/build.go
@@ -4,46 +4,89 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/magefile/mage/sh"
 )
 
+const (
+	amd64 = "amd64"
+)
+
+var errUnknownTarget = errors.New("could not determine zig target")
+
+// Helper function to determine the Zig target triple based on OS and architecture.
+func getTargetTriple(goos, goarch string) (string, error) {
+	switch goos {
+	case "linux":
+		if goarch == amd64 {
+			return "x86_64-linux-gnu", nil
+		} else if goarch == "arm64" {
+			return "aarch64-linux-gnu", nil
+		}
+	case "windows":
+		if goarch == amd64 {
+			return "x86_64-windows-gnu", nil
+		} else if goarch == "arm64" {
+			return "aarch64-windows-gnu", nil
+		}
+	}
+
+	return "", fmt.Errorf("%w: %s/%s", errUnknownTarget, goos, goarch)
+}
+
+// TODO: someday add mussle
+
 // Build a Python wheel.
-func Build(target string) error {
-	env := "build/.venv"
+func Build(goos, goarch string) error {
+	// env := os.mkTemp
+	// defer os.RemoveAll(tmp)
+
+	env, err := filepath.Abs("build/.venv")
+	if err != nil {
+		return err
+	}
+
 	if err := sh.RunV("python3", "-mvenv", env); err != nil {
 		return err
 	}
 
 	defer os.RemoveAll(env)
 
-	pip := env + "/bin/pip"
-	python := env + "/bin/python"
+	pip := filepath.Join(env, "bin", "pip")
+	python := filepath.Join(env, "bin", "python")
 
 	if err := sh.RunV(pip, "install", "build", "ziglang"); err != nil {
 		return err
 	}
 
-	absPython, err := filepath.Abs(python)
-	if err != nil {
-		return err
-	}
-
 	environmentVariables := map[string]string{
-		"GOOS":   "linux", // runtime.GOOS,
-		"GOARCH": "amd64", // runtime.GOARCH,
+		"GOOS":   goos,   // runtime.GOOS,
+		"GOARCH": goarch, // runtime.GOARCH,
 	}
 
 	// Set Zig as the C compiler for cross-compilation
 	// If we are on Mac and targeting Mac we don't need Zig.
-	if !(runtime.GOOS == "darwin" && strings.HasSuffix(target, "-macos")) {
-		zigCC := absPython + " -mziglang cc -target " + target
+	if goos == "darwin" {
+		if runtime.GOOS != "darwin" {
+			return errors.New("unsupported")
+		}
+	} else {
+		target, err := getTargetTriple(goos, goarch)
+		if err != nil {
+			return err
+		}
+
+		zigCC := python + " -mziglang cc -target " + target
 		environmentVariables["CC"] = zigCC
 	}
+	// runtime.GOOS == "darwin" &&
+
+	// strings.HasSuffix(target, "-macos")) {
 
 	if err := sh.RunWithV(
 		environmentVariables,
