ffe4d17606f400ccc3b4ee76af8ee1974acea4fb
nojaf <florian.verdonck@outlook.com>
2024-10-15 15:17:05 +0000

    Install uv in dev container
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,3 +5,9 @@ sudo chown -R $(id -u):$(id -g) /var/cache/go
 
 # Install precommit (https://pre-commit.com/)
 pre-commit install -t pre-commit -t prepare-commit-msg
+
+# uv autocomplete
+echo 'eval "$(uv generate-shell-completion zsh)"' >> ~/.zshrc
+
+# uvx autocomplete
+echo 'eval "$(uvx --generate-shell-completion zsh)"' >> ~/.zshrc
