c80280417d53fd3d46f921cf3a2570e6ac3aa7c5
nojaf <florian.verdonck@outlook.com>
2024-10-15 15:17:22 +0000

    Update confige target to use uv. Add psycopg2-binary as dependency.
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- magefiles/configure.go
+++ magefiles/configure.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 
@@ -11,63 +12,102 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-func pipInstall(args ...string) error {
-	allArgs := append([]string{"install"}, args...)
+// copyFile copies a file from src to dst, preserving file permissions.
+func copyFile(srcFile, dstFile string) error {
+	// Open the source file
+	src, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
 
-	return sh.RunV("pip", allArgs...)
+	// Create the destination file (create directory if needed)
+	dst, err := os.Create(dstFile)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy file content
+	if _, err := io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	// Set the same file permissions
+	info, err := os.Stat(srcFile)
+	if err != nil {
+		return err
+	}
+
+	return os.Chmod(dstFile, info.Mode())
 }
 
-func tar(args ...string) error {
-	return sh.RunV("tar", args...)
+// CopyDirectory recursively copies a source directory to a destination directory.
+func copyDirectory(srcDir, destDir string) error {
+	// Walk through the source directory
+	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Create the destination path by replacing the source base path with the destination path
+		relPath, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(destDir, relPath)
+
+		// Check if it's a directory
+		if info.IsDir() {
+			// Create the directory in the destination if it doesn't exist
+			if err := os.MkdirAll(destPath, info.Mode()); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		// Otherwise, copy the file
+		return copyFile(path, destPath)
+	})
 }
 
 // Configure development environment.
 func Configure() error {
 	mg.Deps(Repo.Init)
 
+	// create virtual environment using uv
+	if err := sh.Run("uv", "venv"); err != nil {
+		return err
+	}
+
 	// Install our Python package and its dependencies
-	if err := pipInstall("-e", "."); err != nil {
+	if err := sh.RunV("uv", "pip", "install", "-e", "."); err != nil {
 		return err
 	}
 
-	// Install the dreaded psycho
-	if err := pipInstall("psycopg2-binary"); err != nil {
+	mlflowDir, err := filepath.Abs(filepath.Join(".venv", "lib", "python3.8", "site-packages", "mlflow"))
+	if err != nil {
 		return err
 	}
 
-	// Archive the MLFlow pre-built UI
-	if err := tar(
-		"-C", "/usr/local/python/current/lib/python3.8/site-packages/mlflow",
-		"-czvf",
-		"./ui.tgz",
-		"./server/js/build",
-	); err != nil {
-		return err
-	}
+	jsBuild := filepath.Join(mlflowDir, "server", "js", "build")
 
 	mlflowRepoPath, err := filepath.Abs(MLFlowRepoFolderName)
 	if err != nil {
 		return err
 	}
 
-	// Add the UI back to it
-	if err := tar(
-		"-C", mlflowRepoPath,
-		"-xzvf", "./ui.tgz",
-	); err != nil {
+	outputJsBuild := filepath.Join(mlflowRepoPath, "server", "js", "build")
+
+	// Copy the MLFlow pre-built UI into .mlflow.repo
+	if err := copyDirectory(jsBuild, outputJsBuild); err != nil {
 		return err
 	}
-
-	// Remove tar file
-	tarPath, err := filepath.Abs("ui.tgz")
-	if err != nil {
-		return err
-	}
-
-	defer os.Remove(tarPath)
 
 	// Install it in editable mode
-	if err := pipInstall("-e", mlflowRepoPath); err != nil {
+	if err := sh.Run("uv", "pip", "install", "-e", mlflowRepoPath); err != nil {
 		return err
 	}
 
