face4bccb6dfd4f584c556b1a3a6e6ad41868567
nojaf <florian.verdonck@outlook.com>
2024-11-06 13:00:57 +0000

    Add initial benchmarks
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

---
+++ benchmarks/k6SearchRunPerfScript.js
@@ -0,0 +1,30 @@
+import http from "k6/http";
+
+// Set base url from environment variable, the variable ' -e MLFLOW_TRACKING_URI = xxxx ' must be added to command line argument
+const base_url = __ENV.MLFLOW_TRACKING_URI + "/api/2.0/mlflow";
+
+if (!base_url.startsWith("http")) {
+    throw new Error("MLFLOW_TRACKING_URI must be a valid URL, starting with http(s)");
+}
+
+function searchRuns() {
+    // 37 was chosen as experiment number with 23525 runs
+    const experiment_id = "37";
+
+    const run_response = http.post(
+        `${base_url}/runs/search`,
+        JSON.stringify({
+            experiment_ids: [experiment_id],
+            max_results: 1000
+        }),
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+}
+
+export default function () {
+    searchRuns()
+}
