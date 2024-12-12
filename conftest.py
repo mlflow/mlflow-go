6ec47f58b663cbfafcd5098f5f950dfa606236cf
nojaf <florian.verdonck@outlook.com>
2024-12-12 18:38:10 +0100

    Except store ones

--- conftest.py
+++ conftest.py
@@ -25,14 +25,14 @@ def pytest_configure(config):
             "tests.tracking.integration_test_utils._init_server",
             "tests/override_server.py",
         ),
-        (
-            "mlflow.store.tracking.sqlalchemy_store.SqlAlchemyStore",
-            "tests/override_tracking_store.py",
-        ),
-        (
-            "mlflow.store.model_registry.sqlalchemy_store.SqlAlchemyStore",
-            "tests/override_model_registry_store.py",
-        ),
+        # (
+        #     "mlflow.store.tracking.sqlalchemy_store.SqlAlchemyStore",
+        #     "tests/override_tracking_store.py",
+        # ),
+        # (
+        #     "mlflow.store.model_registry.sqlalchemy_store.SqlAlchemyStore",
+        #     "tests/override_model_registry_store.py",
+        # ),
         # This test will patch some Python internals to invoke an internal exception.
         # We cannot do this in Go.
         (
